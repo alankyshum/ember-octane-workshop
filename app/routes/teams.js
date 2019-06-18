a297bf28542f685979d7ce9c47edffba649fa3ee
Alan Shum <alan200994@gmail.com>
2019-06-18 14:21:07 -0700

    auto login auto logout route

--- app/routes/teams.js
+++ app/routes/teams.js
@@ -1,4 +1,18 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default class TeamsRoute extends Route {
+  @service auth;
+
+  async beforeModel(transition) {
+    await super.beforeModel(transition);
+    if (!this.auth.currentUserId) {
+
+      /**
+       * NOTES: Do not use `replaceWith`, otherwise the history will be affected,
+       * then users cannot go back and forward anymore
+       */
+      this.transitionTo('/login');
+    }
+  }
 }

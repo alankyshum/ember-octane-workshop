9c6e5231d0e3a478199f60498cfff37e6f93c9f6
Alan Shum <alan200994@gmail.com>
2019-06-18 16:23:25 -0700

    test visiting login route

--- tests/acceptance/login-test.js
+++ tests/acceptance/login-test.js
@@ -2,23 +2,91 @@ import { module, test, skip } from 'qunit';
 import { visit, currentURL, fillIn, click, pauseTest } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 import StubbedAuthService from '../stubs/auth-service';
+import Pretender, { ResponseHandler } from 'pretender';
+
+/**
+ *
+ * @param {any} body
+ * @returns {ResponseHandler}
+ */
+function jsonResponse(body) {
+  return function() {
+    return [200, {}, JSON.stringify(body)];
+  };
+}
+
+/**
+ * @this {Pretender}
+ */
+function setupServer() {
+  this.get(
+    '/api/users',
+    jsonResponse([
+      { id: 1, name: 'Sample McFixture' },
+      { id: 2, name: 'Testy Assertington' },
+    ])
+  );
+  this.get(
+    '/api/users/:id',
+    jsonResponse({ id: 1, name: 'Sample McFixture' })
+  );
+  this.get(
+    '/api/teams',
+    jsonResponse([
+      {
+        id: 'gh',
+        name: 'GitHub',
+      },
+    ])
+  );
+  this.get(
+    '/api/teams/gh',
+    jsonResponse({
+      id: 'gh',
+      name: 'GitHub',
+      channels: [
+        {
+          id: 'prs',
+          name: 'Pull Requests',
+        },
+      ],
+    })
+  );
+  this.get(
+    '/api/teams/gh/channels/prs',
+    jsonResponse({
+      id: 'prs',
+      name: 'Pull Requests',
+    })
+  );
+}
+
 
 module('Acceptance | login', function(hooks) {
   setupApplicationTest(hooks);
+
+  let server;
+  hooks.beforeEach(function() {
+    server = new Pretender(setupServer);
+  });
+
+  hooks.afterEach(function() {
+    server && server.shutdown();
+    server = null;
+  });
 
   hooks.beforeEach(function() {
     this.owner.register('service:auth', StubbedAuthService);
   });
 
-  skip('visiting /login', async function(assert) {
+  test('visiting /login', async function(assert) {
     const auth = this.owner.lookup('service:auth');
     auth._setUserId();
     await visit('/login');
     assert.equal(currentURL(), '/login', 'it show /login page');
 
-    await fillIn('[data-test-login="user-select"]', '2');
-    await click('[data-test-login="signin-button"]');
-    // await click('[type="submit"]');
+    await fillIn('select', '1');
+    await click('[type="submit"]');
 
     assert.equal(currentURL(), '/teams/linkedin/recruitings');
   });
